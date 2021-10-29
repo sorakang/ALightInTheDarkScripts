3cb4cea32906b1626f9a68fb05c636eab1329d2c
sorakang <75961093+sorakang@users.noreply.github.com>
2021-10-29 08:49:54 -0700

    Update LevelManager.cs

--- LevelManager.cs
+++ LevelManager.cs
@@ -2,6 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// public class LevelManager : MonoBehaviour
+// {
+//     public static LevelManager instance;
+//     public static CameraFollow cameraFollow;
+
+//     public Transform respawnPoint;
+//     public GameObject playerPrefab;
+
+//     public Camera cam;
+
+//     private void Awake()
+//     {
+//         if(instance == null)
+//         {
+//         instance = this;
+//         }
+//         else
+//         {
+//             Destroy(gameObject);
+//         }
+//     }
+
+//     public void Respawn()
+//     {
+//         var newPlayer = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+//         cameraFollow.playerTransform = newPlayer.transform;
+//     }
+// }
+
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
@@ -12,13 +41,26 @@ public class LevelManager : MonoBehaviour
 
     public Camera cam;
 
+    // assign first player game object
+    public static GameObject player;
+
     private void Awake()
     {
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     public void Respawn()
     {
-        Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+        // assign instantiated player object
+        player = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
     }
 }

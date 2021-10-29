2d6d02fd06d891912efd9d12b2159c8cc3ea8988
sorakang <75961093+sorakang@users.noreply.github.com>
2021-10-29 09:07:03 -0700

    Update PlayerDeath.cs

--- PlayerDeath.cs
+++ PlayerDeath.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
+    private bool destroying;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Enemy"))
+        if (!destroying && collision.gameObject.CompareTag("Enemy"))
         {
+            destroying = true;
+
             Destroy(gameObject);
             LevelManager.instance.Respawn();
         }
     }
+
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     if(collision.gameObject.CompareTag("Enemy"))
+    //     {
+    //         Destroy(gameObject);
+    //         LevelManager.instance.Respawn();
+    //     }
+    // }
 }

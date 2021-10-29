959a41e91fc7f8e79e2c238d27c3b06b1b947cdd
sorakang <75961093+sorakang@users.noreply.github.com>
2021-10-29 08:34:23 -0700

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -2,26 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// public class CameraFollow : MonoBehaviour
+// {
+//     public Transform playerTransform;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+//     }
+
+//     // Update is called once per frame
+//     void LateUpdate()
+//     {
+//         //store cureent camera's position in variable temp
+//         Vector3 temp = transform.position;
+
+//         //set camera's position x = player's position x
+//         temp.x = playerTransform.position.x;
+
+//         //set camera's temp position to camera's position
+//         transform.position = temp;
+//     }
+// }
+
 public class CameraFollow : MonoBehaviour
 {
-    private Transform playerTransform;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+    public Transform playerTransform;
 
     // Update is called once per frame
     void LateUpdate()
     {
-        //store cureent camera's position in variable temp
-        Vector3 temp = transform.position;
+        if (playerTransform != null)
+        {
+            Vector3 temp = transform.position;
 
-        //set camera's position x = player's position x
-        temp.x = playerTransform.position.x;
+            temp.x = playerTransform.position.x;
 
-        //set camera's temp position to camera's position
-        transform.position = temp;
+            transform.position = temp;
+        }
+        else
+        {
+            GetPlayer();
+        }
+    }
+
+    private void GetPlayer()
+    {
+        playerTransform = LevelManager.player.transform; // get it from the level manager
     }
 }
